ae7cf9eba353163f5e47d9b1725900062bdafe07
Nick G <54913349+NickG-Math@users.noreply.github.com>
2020-11-22 19:56:01 -0600

    Improved documentation

--- source/Relations.h
+++ source/Relations.h
@@ -11,7 +11,7 @@ struct relations_base {
 	///By default 0, but set to 1 in a specialization if you can guarantee that the product of monomials after relations are applied is a monomial (as opposed to polynomial)
 	inline static const bool product_of_monomials_is_monomial=0;
 
-	///Compute degree based on the powers vector and relations
+	///Compute degree based on the exponent vector and relations
 	static int compute_degree(const std::vector<int>&) { return -1; }
 
 	///Apply relations on monomial
@@ -22,13 +22,13 @@ struct relations_base {
 	template<typename polynomial_ptr>
 	static void apply_polynomial(polynomial_ptr) {};
 
-	///Return max powers vector corresponding to polynomial of given degree and number of variables
-	static std::vector<int> max_powers(int, int);
+	///Return max exponent vector corresponding to polynomial of given degree and number of variables
+	static std::vector<int> max_exponent(int, int);
 
-	///Apply permutation on powers vector
+	///Apply permutation on exponent vector
 	static std::vector<int> permute(const std::vector<int>&, const std::vector<char>&);
 
-	///Actual number of variables given length of powers vector
+	///Actual number of variables given length of exponent vector
 	static int true_variables(int n);
 };
 
@@ -36,55 +36,69 @@ struct relations_base {
 ///Class when no relations are needed
 struct norelations : relations_base {
 
+	///Product of monomials with no relations is a monomial
 	inline static const bool product_of_monomials_is_monomial = 1;
-
-	static int compute_degree(const std::vector<int>& powers) {
-		return sum(powers);
+	
+	///The degree of a monomial with given exponent
+	static int compute_degree(const std::vector<int>& exponent) {
+		return sum(exponent);
 	}
 
+	///Apply relations on monomial (i.e. do nothing)
 	template<typename monomial_ptr>
 	static void apply_monomial(monomial_ptr) {}
 
+	///Apply relations on polynomial  (i.e. do nothing)
 	template<typename polynomial_ptr>
 	static void apply_polynomial(polynomial_ptr) {};
 
-	static std::vector<int> max_powers(int variables, int degree) {
+	///Return max exponent vector corresponding to polynomial of given degree and number of variables
+	static std::vector<int> max_exponent(int variables, int degree) {
 		return std::vector<int>(variables, degree);
 	}
 
+	///Apply permutation on exponent vector
 	static std::vector<int> permute(const std::vector<int>& v, const std::vector<char>& perm) {
 		return apply_permutation(v, perm);
 	}
 
+	///Actual number of variables given length of exponent vector (i.e. x_1,...,x_n and n is that number)
 	static int true_variables(int n) { return n; }
 };
 
 ///Class when we have variables \f$x_1,...,x_n,y_1,...,y_n\f$ and the relations are: \f$y_i^2=y_i\f$
 struct halfidempotent : relations_base {
+	///Product of monomials with half idempotent relations is a monomial
 	inline static const bool product_of_monomials_is_monomial = 1;
 
-	static int compute_degree(const std::vector<int>& powers) {
-		return sum(powers,powers.size()/2);
+	///The degree of a monomial with given exponent
+	static int compute_degree(const std::vector<int>& exponent) {
+		return sum(exponent,exponent.size()/2);
 	}
 
+	///Apply relations on monomial
 	template<typename monomial_ptr>
 	static void apply_monomial(monomial_ptr m) {
-		for (int i = m->powers.size() / 2; i < m->powers.size(); i++)
-			m->powers[i] = std::min(m->powers[i], 1);
+		for (int i = m->exponent.size() / 2; i < m->exponent.size(); i++)
+			m->exponent[i] = std::min(m->exponent[i], 1);
 	}
 
+	///Apply relations on polynomial
 	template<typename polynomial_ptr>
 	static void apply_polynomial(polynomial_ptr) {};
 
-	static std::vector<int> max_powers(int variables, int degree) {
+	///Return max exponent vector corresponding to polynomial of given degree and number of variables
+	static std::vector<int> max_exponent(int variables, int degree) {
 		std::vector<int> max(variables, degree);
 		for (int i = max.size() / 2; i < max.size(); i++)
 			max[i] = 1;
 		return max;
 	}
+	///Apply permutation on exponent vector, separately on the variables x_i and the variables y_i
 	static std::vector<int> permute(const std::vector<int>& v, const std::vector<char>& perm) {
 		return apply_permutation_pieces(v, 2, perm);
 	}
+	///Actual number of variables given length of exponent vector (i.e. x_1,...,x_n,y_1,...,y_n and n is that number)
 	static int true_variables(int n) { return n / 2; }
 };
 }
